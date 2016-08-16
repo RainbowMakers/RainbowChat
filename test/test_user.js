2f68286d1ea3f22a2b00fda95c79eda9663724ea
Mariano Schmidt <marianogabriels@gmail.com>
2016-08-16 00:26:35 -0300

    Changed expect with assert

--- test/test_user.js
+++ test/test_user.js
@@ -2,28 +2,30 @@
 
 var TestHelper = require('./test_helper')
 var User = require('../models/user')
-var expect = require('chai').expect;
+var assert = require('chai').assert;
 
 
 describe("User",function() {
     beforeEach(function() {
         TestHelper.dropDatabase()
     });
+
     var user = new User({ name: "prueba",
                         email: "example@example.com" })
-    it(".initialize",function(){ 
-        expect(user.name).to.equal("prueba")
-        expect(user.email).to.equal("example@example.com")
+
+    it("#initialize",function(){ 
+        assert.equal(user.name,"prueba")
+        assert.equal(user.email,"example@example.com")
     })
 
-    it(".save",function(){
-        expect(user.save()).to.equal(true)
+    it("#save",function(){
+        assert(user.save())
     })
 
     it("User.findOne",function(){
         user.save()
         User.findOne({"email":"example@example.com"},function(doc){
-            expect(doc.email).to.equal("example@example.com")
+            assert.equal(doc.email,"example@example.com")
         })
     })
 })
